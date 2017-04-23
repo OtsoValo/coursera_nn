8dd5d6afb1ad4d91ab708f6a711537835c9c6209
Benjamin Hsu <benjaminhsu@hnt.0270.wlan.wharton.upenn.edu>
2017-04-23 16:51:21 -0400

    Finished HW1

--- hw1/learn_perceptron.m
+++ hw1/learn_perceptron.m
@@ -106,12 +106,15 @@ function [w] = update_weights(neg_examples, pos_examples, w_current)
 w = w_current;
 num_neg_examples = size(neg_examples,1);
 num_pos_examples = size(pos_examples,1);
+
 for i=1:num_neg_examples
+    
     this_case = neg_examples(i,:);
     x = this_case'; %Hint
     activation = this_case*w;
     if (activation >= 0)
-        %YOUR CODE HERE
+        desired_output = 0;
+        w = w - x
     end
 end
 for i=1:num_pos_examples
@@ -119,7 +122,8 @@ for i=1:num_pos_examples
     x = this_case';
     activation = this_case*w;
     if (activation < 0)
-        %YOUR CODE HERE
+        desired_output = 0;
+        w = w + x
     end
 end
 
