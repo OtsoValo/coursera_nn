ea31cacc1b4153eff9705595bb66c295f520a31b
Benjamin Hsu <benjaminhsu@Benjamins-MacBook-Pro.local>
2017-04-30 01:11:11 -0400

    Finished hw4

--- a4/cd1.m
+++ a4/cd1.m
@@ -2,5 +2,26 @@ function ret = cd1(rbm_w, visible_data)
 % <rbm_w> is a matrix of size <number of hidden units> by <number of visible units>
 % <visible_data> is a (possibly but not necessarily binary) matrix of size <number of visible units> by <number of data cases>
 % The returned value is the gradient approximation produced by CD-1. It's of the same shape as <rbm_w>.
-    error('not yet implemented');
+
+	visible_data = sample_bernoulli(visible_data);
+	
+	num_configs = size(visible_data, 2);
+
+    hidden_prob1 = visible_state_to_hidden_probabilities(rbm_w, visible_data);
+    hidden1 = sample_bernoulli(hidden_prob1);
+
+    visible_prob2 = hidden_state_to_visible_probabilities(rbm_w, hidden1);
+    visible2 = sample_bernoulli(visible_prob2);
+
+    hidden_prob2 = visible_state_to_hidden_probabilities(rbm_w, visible2);
+    hidden2 = sample_bernoulli(hidden_prob2);
+
+    ret = zeros(size(rbm_w));
+
+    first = 1/num_configs * hidden1 * visible_data';
+
+    second = 1/num_configs * hidden_prob2 * visible2';
+
+    ret = first - second;
+
 end
--- a4/configuration_goodness.m
+++ a4/configuration_goodness.m
@@ -3,5 +3,18 @@ function G = configuration_goodness(rbm_w, visible_state, hidden_state)
 % <visible_state> is a binary matrix of size <number of visible units> by <number of configurations that we're handling in parallel>.
 % <hidden_state> is a binary matrix of size <number of hidden units> by <number of configurations that we're handling in parallel>.
 % This returns a scalar: the mean over cases of the goodness (negative energy) of the described configurations.
-    error('not yet implemented');
+	num_configs = size(visible_state, 2);
+	num_hidden = size(hidden_state, 1);
+	num_visible = size(visible_state, 1);
+
+    total = 0;
+    for x = 1:num_configs
+    	for y = 1:num_visible
+    		for z = 1:num_hidden
+    			total += visible_state(y, x) * hidden_state(z, x) * rbm_w(z, y);
+    		end
+    	end
+    end
+
+    G = total/num_configs;
 end
